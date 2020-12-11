2eae20908abb065e1a3428f685bb47bcf9e3b82a
oli8484 <64160648+oli8484@users.noreply.github.com>
2020-12-11 15:04:08 +0000

    Add files via upload

---
+++ NeedsToBeAddedToListOfPayments.java
@@ -0,0 +1,24 @@
+public class NeedsToBeAddedToListOfPayments {
+ // gets list of payments for owner 
+    public ArrayList<Payment> getOwnerPayments(Owner o) {
+        ArrayList<Payment> py = new ArrayList<>();
+        for (Payment pay : payments) {
+            if (pay.getProperty().getPropertyOwner().equals(p)) {
+                payments.add(pay);
+            }
+        }
+        return py;
+    }
+    // gets list of payemnts for property
+    public ArrayList<Payment> getPropertyPayments(Property p) {
+        ArrayList<Payment> py = new ArrayList<>();
+        for (Payment pay : payments) {
+            if (pay.getProperty().equals(p)) {
+                payments.add(pay);
+            }
+        }
+        return py;
+    }
+
+
+}
