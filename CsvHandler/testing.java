ac090e14c1ce5c77caf085e99168f6287ff758fd
Hexel6 <kramkowski.piotr@gmail.com>
2020-12-12 19:01:44 +0000

    .

--- CsvHandler/testing.java
+++ CsvHandler/testing.java
@@ -5,7 +5,11 @@ public class testing {
     static ArrayList<String[]> array2 = new ArrayList<String[]>();
 
     public static void main(String[] args) {
+
+        // This loads the csv file "testing" from the default file path into an
+        // arraylist array2
         array2 = csvhandler.deserialiseStorage("testing");
+
         for (int i = 0; i < array2.size(); i++) {
             String[] x = array2.get(i);
             for (int j = 0; j < x.length; j++) {
@@ -22,6 +26,8 @@ public class testing {
         array.add(temp1);
         array.add(temp2);
 
+        // This saves the arraylist "array" into csv called testing in the default file
+        // path
         csvhandler.serialiseStorage(array, "testing");
     }
 }
